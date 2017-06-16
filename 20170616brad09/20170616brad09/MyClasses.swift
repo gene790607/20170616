4f6c8fa6175ab68da23f1843daaa0fd9a8bebafa
陳宥瑜 <chenyouyu@chenyouyude-macbook-pro.local>
2017-06-16 09:49:03 +0800

    a

---
+++ 20170616brad09/20170616brad09/MyClasses.swift
@@ -0,0 +1,25 @@
+//
+//  MyClasses.swift
+//  20170616brad09
+//
+//  Created by 陳宥瑜 on 2017/6/16.
+//  Copyright © 2017年 youyu. All rights reserved.
+//
+
+import Foundation
+
+class MyClass05{
+    
+}
+
+class MyClass07 {
+    var x = 0{
+        willSet{
+            // newValue
+        }
+        didSet{
+            
+        }
+    }
+}
+
